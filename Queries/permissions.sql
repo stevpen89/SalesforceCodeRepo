347aac5b6aaef0c7ef2587f7085b9b780eb383b0
Steven Penfield <stevenpenfield9@gmail.com>
2019-08-27 14:53:28 -0600

    adjusted query

--- Queries/permissions.sql
+++ Queries/permissions.sql
@@ -1,4 +1,4 @@
 SELECT Parent.Name, Parent.Profile.Name, PermissionsRead, PermissionsCreate
 FROM ObjectPermissions
 WHERE SobjectType = 'Sales_Recruiting_Role__c'
-AND (PermissionsCreate = true OR PermissionsEdit)
+AND (PermissionsCreate = true OR PermissionsEdit = true)
